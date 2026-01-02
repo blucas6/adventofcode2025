a7f752b18332b1aa0c04b60430617fdf4bdbc4e3
snowmanparty <benj.lucas07@gmail.com>
2026-01-02 14:23:10 -0500

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,26 +1,30 @@
-import math
 max = 99
-dial = 50
+dial = 50 
 passw = 0
 passw2 = 0
-with open("day1example.txt", "r") as file:
+with open('input.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
         line = line.strip()
         if line:
             dir = -1 if line[0] == 'L' else 1
-            move = dir * int(line[1:])
-            if dial + move < 0:
-                passw2 += math.ceil(((move * -1) - dial) / max)
-            elif dial + move > max:
-                passw2 += math.ceil((move - dial) / max)
-            dial += move
-            while dial < 0:
-                dial += max + 1
-            while dial > max:
+            move = int(line[1:])
+            simple = int(move / (max+1))
+            passw2 += simple
+            move -= simple * (max+1) 
+            orig = int(dial)
+            dial += dir * move
+            if dial > max:
                 dial -= max + 1
-            if dial == 0:
+                if orig != 0 and dial != 0:
+                    passw2 += 1
+            elif dial < 0:
+                dial += max + 1
+                if orig != 0 and dial != 0:
+                    passw2 += 1
+            if move != 0 and dial == 0:
                 passw += 1
-            print(dial, move, passw, passw2)
+                passw2 += 1
+            print(dial, dir*move, simple, passw, passw2)
 print(f'Part 1 Password: {passw}')
 print(f'Part 2 Password: {passw2}')
